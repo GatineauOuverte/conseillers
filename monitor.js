fc89b8958eed242a944800e8b7aff31f0f06f8c0
Eric Leclerc <eric.leclerc@gmail.com>
2012-06-01 20:20:32 -0400

    sauvegarde des résultats de twitter

--- monitor.js
+++ monitor.js
@@ -84,32 +84,63 @@ var monitorFeed = function () {
   });
 };
 
+
 //Twitter Feeds
 var monitorTwitter = function() {
-  var options = {
-      host: 'api.twitter.com'
-    , path: '/1/statuses/user_timeline/MPedneaudJobin.json'
-  };
-  https.get(options, function(res) {
-    res.setEncoding('utf8');
-    console.log("Got response: " + res.statusCode);
-    var buff = '';
-    res.on('data', function(chunk) {
-      buff += chunk.toString();
-    });
-    res.on('end', function() {
-      var tweets = JSON.parse(buff);
-      tweets.forEach(function(tweet){
-        console.log('@' + tweet.user.screen_name + ': ' + tweet.text);
+
+  function saveTweets(tweets, conseiller) {
+    tweets.forEach(function(tweet){
+
+      var instance = new Activity();
+      instance.conseiller = conseiller.first_name + ' ' + conseiller.last_name;
+      instance.title = tweet.user.screen_name;
+      instance.guid = tweet.id_str;
+      instance.posted_on = tweet.created_at;
+      instance.source = 'twitter';
+      instance.url = 'https://twitter.com/ramsey/status/' + tweet.id_str;
+      instance.content = tweet.text;
+      instance.save(function(err) {
+        if (err) { console.error(err); }
+        console.log(tweet.user.screen_name + ' - ' + tweet.text);
       });
+
+      console.log(conseiller.first_name + ' @' + tweet.user.screen_name + ': ' + tweet.text);
     });
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
+  };
+
+  conseillers.forEach(function(conseiller) {
+    if (conseiller.twitter == undefined || conseiller.twitter == '') { return; }
+
+    var options = {
+        host: 'api.twitter.com'
+      , path: '/1/statuses/user_timeline/' + conseiller.twitter + '.json'
+    };
+
+    (function(conseiller){
+      var req = https.get(options, function(res) {
+        res.setEncoding('utf8');
+        console.log("Got response: " + res.statusCode);
+        var buff = '';
+        res.on('data', function(chunk) {
+          buff += chunk.toString();
+        });
+        res.on('end', function() {
+          var tweets = JSON.parse(buff);
+          saveTweets(tweets, conseiller);
+        });
+      });
+      req.on('error', function(e) {
+        console.log("Got error: " + e.message);
+      });
+    })(conseiller);
+
   });
 };
 
+
 //Facebook Feeds
 var monitorFacebook = function() {
+
   var options = {
       host: 'graph.facebook.com'
     , path: '/pedneaudjobin/feed?access_token=' + config.facebook.access_token
