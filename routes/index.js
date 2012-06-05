c15db7f2f38d08311f090edfba50590d2e122b2d
Eric Leclerc <eric.leclerc@gmail.com>
2012-06-04 20:17:15 -0400

    ajout des fichiers manquant pour thème bootstrap

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var Activity = require('../models/activity');
  * GET home page.
  */
 exports.index = function(req, res){
-  Activity.find({}).sort('posted_on', -1).limit(25).run(function(err, activities){
+  Activity.find({}).sort('posted_on', -1).limit(30).run(function(err, activities){
     if (err) throw err
     res.render('index', { activities: activities })
   });
